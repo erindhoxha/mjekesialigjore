19caa18b637af1de6e48a03681f44f79d81ec99d
erindhoxha <hello@erindhoxha.dev>
2025-04-11 21:20:48 +0200

    Update

--- App.tsx
+++ App.tsx
@@ -1,26 +1,21 @@
-import { StatusBar } from 'react-native';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { StatusBar } from "react-native";
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
+// TODO: Status
 
-import { Routes } from './src/routes';
-import { Loading } from './src/components/Loading';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Routes } from "./src/routes";
+import { Loading } from "./src/components/Loading";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   if (!fontsLoaded) {
-    return (
-      <Loading />
-    );
+    return <Loading />;
   }
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
 
       <Routes />
     </GestureHandlerRootView>
