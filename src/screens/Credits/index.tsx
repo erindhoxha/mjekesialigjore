1b7b62abc7a512e0cf8ee2208d3d5b43fe20d50d
erindhoxha <hello@erindhoxha.dev>
2025-04-21 00:58:57 +0300

    Styles of credits

--- src/screens/Credits/index.tsx
+++ src/screens/Credits/index.tsx
@@ -1,22 +1,18 @@
-import { useEffect, useRef, useState } from 'react';
-import { useNavigation } from '@react-navigation/native';
-import { View, ScrollView, Text, Linking, TouchableOpacity } from 'react-native';
-import { HouseLine } from 'phosphor-react-native';
-
-import { Header } from '../../components/Header';
-
-import { styles } from './styles';
+import { useNavigation } from "@react-navigation/native";
+import { View, ScrollView, Text, Linking, TouchableOpacity } from "react-native";
+import { HouseLine } from "phosphor-react-native";
+import { Header } from "../../components/Header";
+import { styles } from "./styles";
 
 export function Credits() {
-
   const { goBack } = useNavigation();
 
   const handlePressPhone = () => {
-    Linking.openURL('tel:+38348377390');
+    Linking.openURL("tel:+38348377390");
   };
 
   const handlePressEmail = () => {
-    Linking.openURL('mailto:mjekesialigjoreapp@gmail.com');
+    Linking.openURL("mailto:mjekesialigjoreapp@gmail.com");
   };
 
   return (
@@ -28,29 +24,35 @@ export function Credits() {
         onPress={goBack}
       />
 
-      <ScrollView
-        contentContainerStyle={styles.textContainer}
-        showsVerticalScrollIndicator={false}
-      >
-        <Text style={styles.h1}>Falenderojmë</Text>
-        <Text style={styles.text}>Dr. Genta Bunjaku</Text>
-        <Text style={styles.text1}>Dr. Flamur Blakaj dhe Sokrat Meksi janë autorët e librit Mjekësia Ligjore</Text>
-        <Text style={styles.text2}>Mund ta gjeni librin ta bleni ne keto pika te shitjës: Libraria Dukagjini, Libraria Buzuku, IML.</Text>
-        <Text style={styles.text}>Erind Hoxha (Zhvillues i aplikacionit)</Text>
-        <View style={styles.contactWrapper}>
-          <Text style={styles.contact}>Mund të na kontaktoni në:</Text>
-          <Text style={styles.contact}>
-          Telefono në:
+      <ScrollView contentContainerStyle={styles.textContainer} showsVerticalScrollIndicator={false}>
+        <View style={styles.textWrapper}>
+          <Text style={styles.text1}>Dr. Flamur Blakaj</Text>
+          <Text style={styles.text1}>Dr. Sokrat Meksi</Text>
+          <Text style={styles.text2}>Autor të librit Mjekësia Ligjore</Text>
+          <Text style={styles.text2}>
+            Mund ta gjeni librin ta bleni ne keto pika te shitjës: Libraria Dukagjini, Libraria Buzuku, IML.
           </Text>
-          <TouchableOpacity onPress={handlePressPhone}>
-            <Text style={[styles.text, styles.link]}>+38348377390</Text>
-          </TouchableOpacity>
-          <Text style={styles.contact}>
-          Dergo e-mail nê:
-          </Text>
-          <TouchableOpacity onPress={handlePressEmail}>
-            <Text style={[styles.text, styles.link]}> mjekesialigjoreapp@gmail.com</Text>
-          </TouchableOpacity>
+        </View>
+        <View style={styles.textWrapper}>
+          <Text style={styles.text}>Dr. Genta Bunjaku</Text>
+          <Text style={styles.text2}>Mjeke Ligjore ne Institutin e Mjekësisë Ligjore në Kosovë</Text>
+        </View>
+        <Text style={styles.text}>Erind Hoxha</Text>
+        <Text style={styles.text2}>Zhvillues i aplikacionit</Text>
+        <View>
+          <Text style={styles.contact1}>Kontakti</Text>
+          <View style={styles.contactInner}>
+            <Text style={styles.contact}>Telefono në:</Text>
+            <TouchableOpacity onPress={handlePressPhone}>
+              <Text style={[styles.text, styles.link]}>+38348377390</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.contactInner}>
+            <Text style={styles.contact}>Dergo e-mail nê:</Text>
+            <TouchableOpacity onPress={handlePressEmail}>
+              <Text style={[styles.text, styles.link]}>mjekesialigjoreapp@gmail.com</Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </ScrollView>
     </View>
--- src/screens/Credits/styles.ts
+++ src/screens/Credits/styles.ts
@@ -1,31 +1,43 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
-import { THEME } from '../../styles/theme';
+import { THEME } from "../../styles/theme";
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: THEME.COLORS.GREY_800,
   },
+  textWrapper: {
+    paddingBottom: 0,
+  },
   text: {
     color: THEME.COLORS.GREY_100,
     fontFamily: THEME.FONTS.REGULAR,
     fontSize: 14,
     lineHeight: 18,
-    marginBottom: 12,
+    marginBottom: 4,
+  },
+  contact1: {
+    color: THEME.COLORS.GREY_100,
+    fontFamily: THEME.FONTS.REGULAR,
+    fontSize: 14,
+    lineHeight: 18,
+    marginBottom: 4,
+    marginTop: 12,
   },
   contact: {
-   color: THEME.COLORS.GREY_300,
-   fontFamily: THEME.FONTS.REGULAR,
-   fontSize: 14,
-   lineHeight: 18,
-   marginBottom: 12,
- },
-  contactWrapper: {
-   borderTopWidth: 1,
-   borderTopColor: THEME.COLORS.GREY_300,
-   paddingTop: 12,
-   marginTop: 12,
+    color: THEME.COLORS.GREY_300,
+    fontFamily: THEME.FONTS.REGULAR,
+    fontSize: 12,
+    lineHeight: 18,
+    marginBottom: 4,
+    display: "flex",
+  },
+  contactInner: {
+    display: "flex",
+    alignItems: "center",
+    gap: 4,
+    flexDirection: "row",
   },
   h1: {
     color: THEME.COLORS.GREY_100,
@@ -40,6 +52,13 @@ export const styles = StyleSheet.create({
     lineHeight: 18,
     marginBottom: 2,
   },
+  text3: {
+    color: THEME.COLORS.GREY_300,
+    fontFamily: THEME.FONTS.REGULAR,
+    fontSize: 14,
+    lineHeight: 18,
+    marginBottom: 2,
+  },
   text2: {
     color: THEME.COLORS.GREY_300,
     fontFamily: THEME.FONTS.REGULAR,
@@ -48,11 +67,11 @@ export const styles = StyleSheet.create({
     marginBottom: 12,
   },
   textContainer: {
-   flex: 1,
-   padding: 32,
+    flex: 1,
+    padding: 32,
   },
   link: {
     color: THEME.COLORS.BRAND_MID,
-    textDecorationLine: 'underline',
+    textDecorationLine: "underline",
   },
 });
