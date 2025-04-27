d7f4ae48b3da6d799be7f802df383573a58025c1
erindhoxha <hello@erindhoxha.dev>
2025-04-27 02:21:14 +0200

    Added splash screen and app icon

--- App.tsx
+++ App.tsx
@@ -3,8 +3,13 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold, Roboto_500Medium } from "@
 import { Prata_400Regular } from "@expo-google-fonts/prata";
 import { Cardo_700Bold } from "@expo-google-fonts/cardo";
 import { Routes } from "./src/routes";
-import { Loading } from "./src/components/Loading";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.setOptions({
+  duration: 1000,
+  fade: true,
+});
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -16,13 +21,12 @@ export default function App() {
   });
 
   if (!fontsLoaded) {
-    return <Loading />;
+    return null; // Keep the splash screen visible
   }
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-
       <Routes />
     </GestureHandlerRootView>
   );
