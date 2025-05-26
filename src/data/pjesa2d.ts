bf082f8e9f8fc0ddb3f9274d57327fa3d59043f1
erindhoxha <hello@erindhoxha.dev>
2025-05-26 19:06:10 +0200

    Added more questions for d

--- src/data/pjesa2d.ts
+++ src/data/pjesa2d.ts
@@ -4,19 +4,25 @@ export const PJESA_E_DYTE_D = {
   description: "Dëmtimet nutritive, dëmtimet psikike dhe vdekja nga traumat kimike",
   questions: [
     {
-      title: "Cila është festa kombëtare e Shqipërisë?",
-      alternatives: ["Dita e Pavarësisë", "Dita e Flamurit", "Dita e Çlirimit", "Dita e Verës"],
+      title: 'Tek vdekjet nga uria e dhunshme, "forma e njomë" ka dominancë të:',
+      alternatives: ["Atrofisë së timusit", "Atrofi të organevee në përgjithësi", "Edema në trup", "Asnjëra"],
+      correct: 2,
+      explanation:
+        "Tek forma e njomë kemi edema në trup për shkak të retencionit të ujit dhe rënies së shtypjes koloido-osmotike. Ndërsa atrofia e e gjeneralizuar e indeve dhe organeve ndodh tek forma e thatë.",
+    },
+    {
+      title: "Në helmet me veprim lokal bëjnë pjesë:",
+      alternatives: ["Droga dhe alkooli", "Acidet dhe bazat", "Pesticidet dhe cianidet"],
       correct: 1,
+      explanation:
+        "Në helmet me veprim lokal që verpojnë në vendin e parë të kontaktit hynjë lëndët gërryese ose destruktive si acidet dhe bazat. Alkooli, drogat, pesticidet dhe cianidet bëjnë pjesë në helmet me veprim të përgjithshëm që veprojnëpasi thithen në gjak duke u marrë nga goja.",
     },
     {
-      title: "Cila është kënga më e njohur shqiptare?",
-      alternatives: ["Xhamadani Vija Vija", "Moj e Bukura More", "Valle Kosovare", "Kuq e Zi"],
-      correct: 0,
-    },
-    {
-      title: "Cili është vallja tradicionale shqiptare?",
-      alternatives: ["Vallja e Tropojës", "Vallja e Rugovës", "Vallja e Shpatit", "Vallja e Labërisë"],
-      correct: 3,
+      title: "Në helmet me veprim të përgjithshëm bëjnë pjesë:",
+      alternatives: ["Acidet", "Droga", "Baza", "Kripërat e metaleve të rënda"],
+      correct: 1,
+      explanation:
+        "Në helmet me veprim lokal që verpojnë në vendin e parë të kontaktit hynjë lëndët gërryese ose destruktive si acidet, bazat, kripërat e metaleve të rënda etj. Alkooli, drogat, pesticidet dhe cianidet bëjnë pjesë në helmet me veprim të përgjithshëm që veprojnëpasi thithen në gjak duke u marrë nga goja.",
     },
   ],
 };
