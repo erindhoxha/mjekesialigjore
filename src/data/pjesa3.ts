73e4b205dfac4b8c7d1e6d0702508eb19b9da31b
erindhoxha <hello@erindhoxha.dev>
2025-09-21 22:43:17 +0200

    Added more questions

--- src/data/pjesa3.ts
+++ src/data/pjesa3.ts
@@ -4,19 +4,183 @@ export const PJESA_E_TRETE_DHE_E_KATERT = {
   description: "Investigimi mjekoligjor përballë krimeve kundër shëndetit",
   questions: [
     {
-      title: "Cili është sporti më i njohur në Shqipëri?",
-      alternatives: ["Futbolli", "Basketbolli", "Volejbolli", "Tenisi"],
-      correct: 0,
+      title: "Me dhunë fizike nënkuptojmë:",
+      alternatives: [
+        "Kanosjen nga ana e të pandehurit",
+        "Veprimet aktive nga ana e të pandehurit",
+        "Akuzat e ndryshme nga i pandehuri",
+      ],
+      correct: 1,
+      explanation:
+        "Dhuna fizike shfaqet me veprime aktive nga ana e të pandehurit për të kryer marrdhënie seksuale (dhe jo vetëm) me një individ tjetër.",
     },
     {
-      title: "Cili është klubi më i suksesshëm i futbollit në Shqipëri?",
-      alternatives: ["KF Tirana", "Partizani", "Skënderbeu", "Vllaznia"],
-      correct: 0,
+      title: "Koncepti mbi përkufizimin e dhunës seksuale në përgjithësi nënkupton:",
+      alternatives: [
+        "Vetëm bashkimin seksual duke përdorur forcën",
+        "Bashkimin seksual duke përdorur forcën, kanosjen apo mashtrimin me apo pa vullnetin e personit tjetër",
+        "Vetëm kanosjën apo mashtrimin",
+        "Bashkimin seksual duke përdorur forcën, kanosjen apo mashtrimin, kundër vullnetit të personit tjetër.",
+      ],
+      correct: 3,
+      explanation:
+        "Koncepti mbi përkufizimin e dhunës seksuale në përgjithësi nënkupton bashkimin seksual duke përdorur forcën, kanosjen apo mashtrimin, kundër vullnetit të personit tjetër.",
     },
     {
-      title: "Cili është sportisti më i njohur shqiptar?",
-      alternatives: ["Lorik Cana", "Edi Rama", "Ismail Qemali", "Gjergj Kastrioti"],
+      title: "Cila nga këto kategorizohet si vepër e turpshme:",
+      alternatives: [
+        "Bashkimi seksual në rrugë vaginale",
+        "Prekja e organeve seksuale të të miturve",
+        "Bashkimi seksual me rrugë anale",
+        "Shikimi i të miturve.",
+      ],
+      correct: 2,
+      explanation:
+        "Konsiderohet vepër e turpshme të gjitha format e përfitimit të kënaqësisë nga ana e personave të rritur, pa kryer bashkimin seksual të natyrshmëm apo kundërnatyrshëm me të mitur. Forma të tilla janë: prekja e organeve seksuale të të miturve me dorë ose ndonjë pjesë tjetër të trupit nga të rriturit.",
+    },
+    {
+      title: "Kur foshnjën e privon nga jeta babai quhet:",
+      alternatives: ['"Vrasje e priviligjuar"', "Puericid", "Neonaticid"],
+      correct: 1,
+      explanation:
+        "Nëse foshjën e privon nga jeta babi ose ndonjë person tjetër do të quhet fillicid përkatësisht puericid.",
+    },
+    {
+      title: "Shenjë e porsalindjës nuk konsiderohet:",
+      alternatives: ["Vernix caseosa", "Capput succedaneum", "Laguno", "Osifikimi Beclard"],
+      correct: 3,
+      explanation:
+        "Shenjat e porsalindjës janë të jashtme siç janë vernix caseosa, lanugo, umbilikusi dhe të brendshme si meconiumi, caput succedaneum, cephalhematoma. Ndërsa bërdhama e osifikimit Beclard na tregom për pjekurinë e porsalindurit.",
+    },
+    {
+      title: "Cila nga këto nuk është e vërtetë në vërtetimin se foshnjet kanë lindur gjallë:",
+      alternatives: [
+        "Perimetri i kraharorit është 2 cm më i vogël se perimetri i barkut",
+        "Diafragma zakonisht zbret nën hapsirën e gjashtë interkostale",
+        "Mushkëritë e mbushsin mediastunumin si dhe zgavrën e kraharorit",
+        "Në prekje mushkëritë kërcasin, janë të buta, me konsistencë sfungjeroze dhe me ngjyrë rozë",
+      ],
       correct: 0,
+      explanation:
+        "Perimetri i kraharorit është 2 cm më i madh se perimetri i barkut. Të gjitha të tjerat janë të sakta.",
+    },
+    {
+      title: "Në prova të sigurta në përcaktimin e të lindurit gjallë nuk bëjnë pjesë:",
+      alternatives: [
+        "Prova hidrostatike e mushkërive",
+        "Prova e majtës së diametrit të kraharorit dhe abdomenit",
+        "Prova gastro-intestinale",
+      ],
+      correct: 1,
+      explanation:
+        "Në prova të sigurta bëjnë pjesë: Prova hidrostatike e mushkërive dhe prova gastro intestinale. Matja e kafazit të kraharorit dhe abdomenit nuk bënë pjesë ne provë të sigurt por bën pjesë në shenja ndihmëse-orjentuese.",
+    },
+    {
+      title: "Me provë pozitive të rreme kemi të bëjme në rastet e :",
+      alternatives: ["Kufomat e ndodhura në flakë", "Mushkërive të ngrira", "Sufokimit"],
+      correct: 1,
+      explanation:
+        "Me provë pozitive të rreme kemi të bëjmë në rastet e: fillimit të kalbëzimit kufomor; mushkëritë e ngrira; insulfimit të ajrit gjatë reanimimit etj",
+    },
+    {
+      title: "Me provë negative të rreme kemi të bëjmë në rastet e:",
+      alternatives: ["Fillimit të kalbëzimit kufomor", "Pneumonisë", "Insuflimit të ajrit gjatë reanimimit"],
+      correct: 0,
+      explanation:
+        "Me provë negative të rreme kemi të bëjmë në rastet e: pneumonisë, sufokimit, kufomat e ndodhura në flakë etj.",
+    },
+    {
+      title: "Triada e shenjave: gjakderdhje retinale, gjakderdhje subaraknoidale dhe hematoma subdurale; shfaqen tek:",
+      alternatives: [
+        "Sindroma e fëmijës së rrahur",
+        "Sindroma e vrasjes nga zemërimi",
+        "Sindroma e tundjes së fëmijës",
+        "Sindroma Munchausen",
+      ],
+      correct: 2,
+      explanation:
+        "Triada e shenjave: gjakderdhje retinale, gjakderdhje subaraknoidale dhe hematoma subdurale; shfaqen tek Sindroma e tundjes së fëmijës.",
+    },
+    {
+      title: "Pikëzimet hemorragjike ne zemer, mushkëri, timus si fenomene agonale i hasim tek:",
+      alternatives: [
+        "Sindroma e fëmijës së rrahur",
+        "Sindroma e vrasjes nga zemërimi",
+        "Sindroma e tundjes së fëmijës",
+        "Sindroma e vdekjes së papritur të foshnjeve",
+      ],
+      correct: 3,
+      explanation:
+        "Pikëzimet hemorragjike ne zemer, mushkëri, timus si fenomene agonale i hasim tek Sindroma e vdekjes së papritur të foshnjeve.",
+    },
+    {
+      title: "Në triadën e simboleve të identitetit nuk bënë pjesë",
+      alternatives: ["Gjurmët e gishtërinjëve", "Frakturat e vjetra", "Profili gjenetik", "Veçoritë e dhëmbëve"],
+      correct: 1,
+      explanation:
+        "Në triadën e simboleve të identitetit bëjnë pjesë gjurmët e gishtërinjëve, profili gjenetik ADN dhe veçoritë e dhëmbëve.",
+    },
+    {
+      title:
+        'Sipas llojeve të objekteve identifikuese, bazuar në principet e "Mark Theory" identifikimi "in toto" paraqet:',
+      alternatives: [
+        "Identifikimin e një objekti në bazë të së pjesshmës, e shkëputur nga e tëra si pasojë e veprimit të faktorëve të ndryshem",
+        "Identifikimin e objektit në bazë të së tërës (një trup i plotë njerëzor)",
+        "Identifikimi i objektit në bazë të gjurmëve dhe principit të Locardi-t.",
+      ],
+      correct: 1,
+      explanation:
+        'Sipas llojeve të objekteve identifikuese, bazuar në principet e "Mark Theory" identifikimi "in toto" paraqet: identifikimin e objektit në bazë të së tërës (një trup i plotë njerëzor).',
+    },
+    {
+      title:
+        'Sipas llojeve të objekteve identifikuese, bazuar në principet e "Mark Theory" identifikimi "in situ" paraqet:',
+      alternatives: [
+        "Identifikimin e një objekti në bazë të së pjesshmës, e shkëputur nga e tëra si pasojë e veprimit të faktorëve të ndryshem",
+        "Identifikimin e objektit në bazë të së tërës (një trup i plotë njerëzor)",
+        "Identifikimi i objektit në bazë të gjurmëve dhe principit të Locardi-t",
+      ],
+      correct: 0,
+      explanation:
+        'Sipas llojeve të objekteve identifikuese, bazuar në principet e "Mark Theory" identifikimi "in situ" paraqet: identifikimin e një objekti në bazë të së pjesshmës, e shkëputur nga e tëra si pasojë e veprimit të faktorëve të ndryshem.',
+    },
+    {
+      title:
+        'Sipas llojeve të objekteve identifikuese, bazuar në principet e "Mark Theory" identifikimi "indirectum" paraqet:',
+      alternatives: [
+        "Identifikimin e një objekti në bazë të së pjesshmës, e shkëputur nga e tëra si pasojë e veprimit të faktorëve të ndryshem",
+        "Identifikimin e objektit në bazë të së tërës (një trup i plotë njerëzor)",
+        "Identifikimi i objektit në bazë të gjurmëve dhe principit të Locardi-t",
+      ],
+      correct: 2,
+      explanation:
+        'Sipas llojeve të objekteve identifikuese, bazuar në principet e "Mark Theory" identifikimi "indirectum" paraqet: identifikimi i objektit në bazë të gjurmëve dhe principit të Locardi-t.',
+    },
+    {
+      title:
+        'Cili nga llojet e objekteve identifikuese, bazuar në principet e "mark theory" vepron në bazë të principit të Locardit?',
+      alternatives: ['Identifikimi "in situ"', 'Identifikimi "indirectum"', 'Identifikimi "in toto"'],
+      correct: 1,
+      explanation:
+        'Sipas llojeve të objekteve identifikuese, bazuar në principet e "Mark Theory" identifikimi "indirectum" paraqet: identifikimi i objektit në bazë të gjurmëve dhe principit të Locardi-t.',
+    },
+    {
+      title: "Në metodat shkencore të identifikimit nuk përfshihet:",
+      alternatives: [
+        "Radiografia e eshtrave",
+        "Shenjat daktiloskopike",
+        "Pamja e drejtpërsëdrejti e fytyrës",
+        "Lëkura.",
+      ],
+      correct: 2,
+      explanation:
+        "Pamja drejtpërsëdrejti e fytyrës bën pjesë në metodat jo shkencore të identifikimit të kufomave të panjohura.",
+    },
+    {
+      title: "Në prova të tjera shkencore për identifikim bëjnë pjesë:",
+      alternatives: ["Raca", "Gjinia", "Mjekrra", "Lëkura"],
+      correct: 3,
+      explanation: "Në prova të tjera shkencore për identifikim bëjë pjesë: qimet, sperma, lëkura etj.",
     },
   ],
 };
